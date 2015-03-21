1df15d7d592838ec715cc6137c07b6bbf447d7ea
JuanDMeGon <contacto@juandmegon.com>
2015-03-21 10:24:30 -0500

    Creando migraciones de los modelos

--- database/migrations/2015_03_21_135504_CrearTablaAlbumes.php
+++ database/migrations/2015_03_21_135504_CrearTablaAlbumes.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CrearTablaAlbumes extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,15 +12,13 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('usuarios', function(Blueprint $table)
+		Schema::create('albumes', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->string('nombre');
-			$table->string('email')->unique();
-			$table->string('password', 60);
-			$table->string('pregunta');
-			$table->string('respuesta');
-			$table->rememberToken();
+			$table->string('descripcion');
+			$table->integer('usuario_id')->unsigned();
+			$table->foreign('usuario_id')->references('id')->on('usuarios');
 			$table->timestamps();
 		});
 	}
@@ -32,7 +30,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('usuarios');
+		Schema::drop('albumes');
 	}
 
 }
--- database/migrations/2015_03_21_135527_CrearTablaFotos.php
+++ database/migrations/2015_03_21_135527_CrearTablaFotos.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CrearTablaFotos extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,15 +12,14 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('usuarios', function(Blueprint $table)
+		Schema::create('fotos', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->string('nombre');
-			$table->string('email')->unique();
-			$table->string('password', 60);
-			$table->string('pregunta');
-			$table->string('respuesta');
-			$table->rememberToken();
+			$table->string('descripcion');
+			$table->string('ruta');
+			$table->integer('album_id')->unsigned();
+			$table->foreign('album_id')->references('id')->on('albumes');
 			$table->timestamps();
 		});
 	}
@@ -32,7 +31,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('usuarios');
+		Schema::drop('fotos');
 	}
 
 }
